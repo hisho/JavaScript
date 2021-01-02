c6be939403c5bf675e40269e36125638c054a467
Hisho <>
2021-01-03 01:48:50 +0900

    🔧 add postcss-import-resolver

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
+const path = require('path');
+const resolver = require('postcss-import-resolver');
+
 module.exports = {
   plugins: {
-    "postcss-import": {},
+    'postcss-import': {
+      resolve: resolver({
+        alias: {
+          'src': path.resolve('./src'),
+        },
+      }),
+    },
     "postcss-nested": {},
     tailwindcss: {},
     autoprefixer: {},
