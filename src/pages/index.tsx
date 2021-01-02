e36d0f57a9748b0d49e7eb7e4e61c09ab6ee80e4
Hisho <>
2021-01-03 03:55:03 +0900

    ✨ udpate index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import {ProcessedOptions} from 'html-webpack-plugin';
 import {newRenderToStaticMarkup} from "src/hooks";
+import {ButtonLink, Section} from "src/components";
 
 export default ({htmlWebpackPlugin}: ProcessedOptions) => {
   return newRenderToStaticMarkup(
+    <>
       テスト
+      <Section>
+        <div className="flex flex-col">
+          <ButtonLink>TOP</ButtonLink>
+          <ButtonLink href="template">テンプレート</ButtonLink>
+        </div>
+      </Section>
+    </>
   )(htmlWebpackPlugin);
 };
