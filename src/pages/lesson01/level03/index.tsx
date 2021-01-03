a0bb740d2287708df3af6123d96c9ca5d18a1b67
Naoki Yamamoto <mtbk4919naoki@gmail.com>
2021-01-03 21:32:03 +0900

    fix: class名を修正

--- src/pages/lesson01/level03/index.tsx
+++ src/pages/lesson01/level03/index.tsx
@@ -8,9 +8,9 @@ export default ({htmlWebpackPlugin}: ProcessedOptions) => {
     <>
       <Section>
         <div>
-          <p>.Lesson01Level02Buttonを取得し、1つ目のボタンを押すと<br />bodyの背景色が#3cb371と#ffa500に交互に変わるコードを書け！</p>
-          <p>.Lesson01Level02Buttonを取得し、2つ目のボタンを押すと<br />bodyの背景色が#ffc0cbと#6495edに交互に変わるコードを書け！</p>
-          <p>.Lesson01Level02Buttonを取得し、3つ目のボタンを押すと<br />bodyの背景色が#9370dbと#fffaf0に交互に変わるコードを書け！</p>
+          <p>.Lesson01Level03Buttonを取得し、1つ目のボタンを押すと<br />bodyの背景色が#3cb371と#ffa500に交互に変わるコードを書け！</p>
+          <p>.Lesson01Level03Buttonを取得し、2つ目のボタンを押すと<br />bodyの背景色が#ffc0cbと#6495edに交互に変わるコードを書け！</p>
+          <p>.Lesson01Level03Buttonを取得し、3つ目のボタンを押すと<br />bodyの背景色が#9370dbと#fffaf0に交互に変わるコードを書け！</p>
           <ButtonLink>TOPに戻る</ButtonLink>
           <p className="text-center text-8xl">↓</p>
         </div>
@@ -18,9 +18,9 @@ export default ({htmlWebpackPlugin}: ProcessedOptions) => {
 
       <Section>
         <div>
-          <Button className="Lesson01Level02Button">ボタン1</Button>
-          <Button className="Lesson01Level02Button">ボタン2</Button>
-          <Button className="Lesson01Level02Button">ボタン3</Button>
+          <Button className="Lesson01Level03Button">ボタン1</Button>
+          <Button className="Lesson01Level03Button">ボタン2</Button>
+          <Button className="Lesson01Level03Button">ボタン3</Button>
         </div>
       </Section>
     </>
