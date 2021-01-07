1da490330a01e8de9d6458a64e7f100936caf7c5
Hisho <>
2021-01-07 23:33:32 +0900

    🎉 lesson01 level02

--- src/js/lesson01/level02.ts
+++ src/js/lesson01/level02.ts
@@ -1,2 +1,33 @@
-//you can rename extension to js
-console.log('level02');
+(() => {
+
+  //.Lesson01Level02Button DOMs
+  const buttons = document.querySelectorAll<HTMLElement>('.Lesson01Level02Button');
+
+  //early return when does not exist .Lesson01Level02Button DOM
+  if (buttons.length === 0) return;
+
+  //body DOM
+  const body = document.body;
+
+  //initialize state
+  const state = {
+    press: false
+  }
+
+  //handle click event
+  function clickEvent() {
+    if (state.press) {
+      //change body background-color
+      body.style.backgroundColor = "#3cb371";
+    } else {
+      //change body background-color
+      body.style.backgroundColor = "#ffa500";
+    }
+    //change state
+    state.press = !state.press;
+  }
+
+  //register click event for button DOM
+  buttons.forEach(n => n.addEventListener('click', clickEvent, false));
+
+})()
