cdf5f97a97a6b665a1ab800c38c105c4114ca6fc
Hisho <>
2021-01-08 00:11:32 +0900

    🎉 lesson01 level05

--- src/js/lesson01/level05.ts
+++ src/js/lesson01/level05.ts
@@ -1,2 +1,32 @@
-//you can rename extension to js
-console.log('level05');
+(() => {
+
+  //#Lesson01Level05Button DOM
+  const button = document.getElementById('Lesson01Level05Button');
+
+  //early return when does not exist #Lesson01Level05Button DOM
+  if (!button) return;
+
+  //#Lesson01Level05Color01 DOM
+  const input = document.getElementById('Lesson01Level05Color01') as HTMLInputElement | null;
+
+  //early return when does not exist #Lesson01Level05Color01 DOM
+  if (!input) return;
+
+  //body DOM
+  const body = document.body;
+
+
+  //handle click event
+  function clickEvent() {
+
+    //early return when does not exist #Lesson01Level05Color01 DOM
+    if (!input) return;
+    //change background-color
+    body.style.backgroundColor = input.value;
+
+  }
+
+  //register click event for button DOM
+  button.addEventListener('click', clickEvent, false);
+
+})()
