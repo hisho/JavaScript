1ffb0a8b8832daf2c3cd1092c830677269087de2
MIRAIYA creation <miraiyacreation@gmail.com>
2021-01-08 20:56:02 +0900

    配列が謎！

--- src/js/lesson01/level03.js
+++ src/js/lesson01/level03.js
@@ -1,11 +1,21 @@
 //you can rename extension to js
 const body = document.body;
 const btns = [...document.querySelectorAll('.Lesson01Level03Button')];
-// btns = [].slice.call(btns);
-console.log(btns);
+// console.log(btns);
 
 btns.forEach(btn => {
 
   console.log(btn[0]);
+  btn.addEventListener('click', () => {
+
+    // if()
+    // console.log('click');
+    // body.style.backgroundColor = "#ffa500";
+
+  });
 
 });
+
+// for (let i = 0; i < btns.length; i++) {
+//   console.log(btns[2]);
+// }
