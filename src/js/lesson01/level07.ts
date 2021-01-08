e3e15694ab5fd143468860906ce21fe0d13f4e57
Naoki Yamamoto <mtbk4919naoki@gmail.com>
2021-01-09 00:00:45 +0900

    answer: level07 bindでは解除のほうがうまくいってなかった

--- src/js/lesson01/level07.ts
+++ src/js/lesson01/level07.ts
@@ -7,15 +7,16 @@ const btnSwitch = document.getElementById('Lesson01Level07Button');
 const btnRegister = document.getElementById('Lesson01Level07ButtonRegister');
 const btnUnRegister = document.getElementById('Lesson01Level07ButtonUnRegister');
 
-const switchBgColor = new SwitchBgColor().config({colors:['#ffa500', '#3cb371']});
-
 if(btnSwitch && btnRegister && btnUnRegister) {
+  const switchBgColor = new SwitchBgColor().config({colors:['#ffa500', '#3cb371']});
+  const switchListener = (_evt:MouseEvent) => {switchBgColor.next()};
+
   btnRegister.addEventListener('click', (_evt) => {
-    btnSwitch.addEventListener('click', switchBgColor.next.bind(switchBgColor));
+    btnSwitch.addEventListener('click', switchListener);
   });
 
   btnUnRegister.addEventListener('click', (_evt) => {
-    btnSwitch.removeEventListener('click', switchBgColor.next.bind(switchBgColor));
+    btnSwitch.removeEventListener('click', switchListener);
   });
 }
 
