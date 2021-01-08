698003d486ee94554794e1921debc239670702f6
omi <>
2021-01-08 18:46:17 +0900

    level00 dekita!!!!!!!!!!!!!!!!!!!!!

--- src/js/lesson01/level00.js
+++ src/js/lesson01/level00.js
@@ -1,25 +1,25 @@
 import { Section } from "src/components";
 
 //you can rename extension to js
-console.log('level00');
+console.log('test');
 
 
 const getomi = document.querySelector('.ominoButton');
+// const getomi =document.getElementById('exampleID')
 
-
-console.log('getomi');
+console.log(getomi);
 var nam = 0;
 
-document.addEventListener('click', (getomi) => {
-
+getomi.addEventListener('click', () => {
+console.log(nam);
 
   if (nam == 0) {
     // document.body.style.backgroundColor = '#3cb371';
-    document.body.setAttribute("style", "backgroundcolor:#3cb371;");
+    document.body.setAttribute("style", "background-color:#3cb371;");
     nam = 1;
   } else {
     // document.body.style.backgroundColor = '#ffa500';
-    document.body.setAttribute("style", "backgroundcolor:#ffa500;");
+    document.body.setAttribute("style", "background-color:#ffa500;");
     nam = 0;
   }
 
