fce6bff3bd29a2841679068e970ab0ab29146777
omi <>
2021-01-08 22:32:34 +0900

    level02 clear!!!

--- src/js/lesson01/level02.ts
+++ src/js/lesson01/level02.ts
@@ -1,2 +1,50 @@
 //you can rename extension to js
 console.log('level02');
+const button02: NodeListOf<HTMLElement> = document.querySelectorAll('.Lesson01Level02Button');
+// const getomi =document.getElementById('exampleID')
+
+
+// if(button02) {
+//   console.log(button02);
+//   let nam =0;
+//   for (var i = 0; i < button02.length; i++) {
+
+//       button02[i].addEventListener('click', () => {
+
+//           if (nam == 0) {
+//             // document.body.style.backgroundColor = '#3cb371';
+//             document.body.setAttribute("style", "background-color:#3cb371;");
+//             nam = 1;
+//           } else {
+//             // document.body.style.backgroundColor = '#ffa500';
+//             document.body.setAttribute("style", "background-color:#ffa500;");
+//             nam = 0;
+//           }
+
+//         }, false);
+
+//   }
+
+// }
+
+if(button02) {
+  console.log(button02);
+  let nam =0;
+  button02.forEach((x: HTMLElement) => {
+    x.addEventListener('click', () => {
+
+      if (nam == 0) {
+        // document.body.style.backgroundColor = '#3cb371';
+        document.body.setAttribute("style", "background-color:#3cb371;");
+        nam = 1;
+      } else {
+        // document.body.style.backgroundColor = '#ffa500';
+        document.body.setAttribute("style", "background-color:#ffa500;");
+        nam = 0;
+      }
+
+    }, false);
+  }
+
+  )}
+
