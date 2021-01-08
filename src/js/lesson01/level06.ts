46b7923f2d4f66b8ce35e3480dddd19e23b1d7c5
Naoki Yamamoto <mtbk4919naoki@gmail.com>
2021-01-08 23:46:49 +0900

    answer: level06とlevel07に回答

--- src/js/lesson01/level06.ts
+++ src/js/lesson01/level06.ts
@@ -1,2 +1,18 @@
+import SwitchBgColor from "src/js/lib/SwitchBgColor";
+
 //you can rename extension to js
 console.log('level06');
+
+const button = document.getElementById('Lesson01Level06Button');
+
+if(button) {
+  const switchBgColor = new SwitchBgColor().config({colors:['#3cb371']});
+
+  button.addEventListener('click', {
+    handleEvent(_evt) {
+      switchBgColor.next();
+      _evt.currentTarget?.removeEventListener('click', this);
+      console.log('色が変わった！');
+    }
+  });
+}
--- src/js/lesson01/level07.ts
+++ src/js/lesson01/level07.ts
@@ -1,2 +1,21 @@
+import SwitchBgColor from "src/js/lib/SwitchBgColor";
+
 //you can rename extension to js
 console.log('level07');
+
+const btnSwitch = document.getElementById('Lesson01Level07Button');
+const btnRegister = document.getElementById('Lesson01Level07ButtonRegister');
+const btnUnRegister = document.getElementById('Lesson01Level07ButtonUnRegister');
+
+const switchBgColor = new SwitchBgColor().config({colors:['#ffa500', '#3cb371']});
+
+if(btnSwitch && btnRegister && btnUnRegister) {
+  btnRegister.addEventListener('click', (_evt) => {
+    btnSwitch.addEventListener('click', switchBgColor.next.bind(switchBgColor));
+  });
+
+  btnUnRegister.addEventListener('click', (_evt) => {
+    btnSwitch.removeEventListener('click', switchBgColor.next.bind(switchBgColor));
+  });
+}
+
