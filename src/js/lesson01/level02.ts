918293ce8662b4133109d6c4bd9c10d0347ba893
岩田康平 <iwater0715@gmail.com>
2021-01-16 15:02:45 +0900

    add: level3 くそきたなくね

--- src/js/lesson01/level02.ts
+++ src/js/lesson01/level02.ts
@@ -1,2 +1,13 @@
 //you can rename extension to js
 console.log('level02');
+
+// const level2Btns = document.querySelectorAll('.Lesson01Level02Button')
+// const colors = ['#3cb371', '#ffa500']
+// let idx = 0
+// function changeBodyBgColor() {
+//   document.body.style.backgroundColor = colors[idx]
+//   idx++
+//   if (idx === colors.length)
+//     idx = 0
+// }
+// level2Btns.forEach(btn => btn.addEventListener('click', changeBodyBgColor))
