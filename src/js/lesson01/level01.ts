b46ba2564d81e9ba0c6bfbddaca0a6ca178a75bc
岩田康平 <iwater0715@gmail.com>
2021-01-16 13:49:59 +0900

    level01

--- src/js/lesson01/level01.ts
+++ src/js/lesson01/level01.ts
@@ -1,2 +1,13 @@
 //you can rename extension to js
 console.log('level01');
+
+const level1Btn = document.getElementById('Lesson01Level01Button')
+const colors = ['#3cb371', '#ffa500']
+let idx = 0
+function func() {
+  document.body.style.backgroundColor = colors[idx]
+  idx++
+  if (idx === colors.length)
+    idx = 0
+}
+level1Btn!.addEventListener('click', func)
