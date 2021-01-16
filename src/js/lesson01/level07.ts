1bfd59abc081d3f32ded465085e9f8b37b5f306e
岩田康平 <iwater0715@gmail.com>
2021-01-16 15:56:01 +0900

    add: level7

--- src/js/lesson01/level07.ts
+++ src/js/lesson01/level07.ts
@@ -1,2 +1,21 @@
 //you can rename extension to js
 console.log('level07');
+
+const level7Btn = document.getElementById('Lesson01Level07Button')
+const registerBtn = document.getElementById('Lesson01Level07ButtonRegister')
+const unRegisterBtn = document.getElementById('Lesson01Level07ButtonUnRegister')
+const colors = ['#3cb371', '#ffa500']
+let isRegister = false
+let idx = 0
+
+function changeBodyBgColor() {
+  if (isRegister) {
+    document.body.style.backgroundColor = colors[idx]
+    idx++
+    if (idx === colors.length)
+      idx = 0
+  }
+}
+registerBtn!.addEventListener('click', () => { isRegister = true })
+unRegisterBtn!.addEventListener('click', () => { isRegister = false })
+level7Btn!.addEventListener('click', changeBodyBgColor)
