7e7df8692bb51fd2053e7a905d011da38b19e150
岩田康平 <iwater0715@gmail.com>
2021-01-16 15:45:18 +0900

    add: level6

--- src/js/lesson01/level05.ts
+++ src/js/lesson01/level05.ts
@@ -1,10 +1,10 @@
 //you can rename extension to js
 console.log('level05');
 
-const picker = document.getElementById('Lesson01Level05Color01')
-
-function changeColor(this: HTMLInputElement) {
-  document.body.style.backgroundColor = this.value
-}
-
-picker!.addEventListener('change', changeColor)
+// const picker = document.getElementById('Lesson01Level05Color01')
+//
+// function changeColor(this: HTMLInputElement) {
+//   document.body.style.backgroundColor = this.value
+// }
+//
+// picker!.addEventListener('change', changeColor)
--- src/js/lesson01/level06.ts
+++ src/js/lesson01/level06.ts
@@ -1,2 +1,9 @@
 //you can rename extension to js
 console.log('level06');
+
+// const level6Btn = document.getElementById('Lesson01Level06Button')
+//
+// level6Btn!.addEventListener('click', () => {
+//   console.log(`once`)
+//   document.body.style.backgroundColor = '#3cb371'
+// }, { once: true })
