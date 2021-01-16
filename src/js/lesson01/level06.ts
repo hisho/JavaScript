d07700937ae72b9199574040db636e8d52329b71
Hisho <>
2021-01-16 13:15:30 +0900

    🎉 lesson01 level06

--- src/js/lesson01/level06.ts
+++ src/js/lesson01/level06.ts
@@ -1,2 +1,14 @@
-//you can rename extension to js
-console.log('level06');
+(() => {
+  const button = document.getElementById('Lesson01Level06Button');
+
+  if (!button) return;
+
+  const body = document.body;
+
+  function clickEvent() {
+    body.style.backgroundColor = '#3cb371';
+    console.log('色が変わった');
+  }
+
+  button.addEventListener('click', clickEvent, {once: true});
+})()
